a4bcb5607b19ec98cc414ef9bf409ec69bbfba4c
Vhatever <fserrorka@gmail.com>
2025-04-14 17:50:39 +0300

    tryN2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,22 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const mainTmage = document.getElementById('largeImg');
+  const mainImage = document.getElementById('largeImg');
   const container = document.getElementById('thumbs');
 
-  container.addEventListener('click', (events) => {
-    const target = events.target;
+  container.addEventListener('click', (e) => {
+    const target = e.target;
 
-    if (target.tagName === 'IMG') {
-      const parentLink = target.parentElement;
+    // If the clicked element is the <img>
+    if (target.tagName === 'IMG' && target.parentElement.tagName === 'A') {
+      e.preventDefault();
+      mainImage.src = target.parentElement.href;
+    }
 
-      if (parentLink && parentLink.tagName === 'A') {
-        events.preventDefault();
-        mainTmage.src = parentLink.href;
-      }
+    // If the clicked element is the <a> tag directly
+    if (target.tagName === 'A' && target.href) {
+      e.preventDefault();
+      mainImage.src = target.href;
     }
   });
 });
